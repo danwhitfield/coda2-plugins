bb92ee3a7b3b558154d517b87423a4c0512cc5b4
Dan Whitfield <dwhitfie@amazon.com>
2014-08-11 13:22:24 +0100

    Added support for aligning colons, which can be used in JavaScript literal objects etc...

--- align-equals-coda-plugin-script.pl
+++ align-equals-coda-plugin-script.pl
@@ -9,12 +9,12 @@ my $tab_width   = 4;
 
 if($#lines > 0) {
     my ($line, $longest_line, $equals_operator, $split_regex, $padding, $line_number, $line_start, $line_end);
-    
-    foreach $line (@lines) {
-        next if $line !~ m/.+=/;
 
-        ($equals_operator)      = ($line =~ m/([\.\+\-]?=[\>\&\~]?)/);
-        ($split_regex           = $equals_operator) =~ s/([\.\+\-\=\>\&])/\\$1/g;        
+    foreach $line (@lines) {
+        next if $line !~ m/.+[=\:]/;
+
+        ($equals_operator)      = ($line =~ m/([\.\+\-]?[=\:][\>\&\~]?)/);
+        ($split_regex           = $equals_operator) =~ s/([\.\+\-\=\>\&])/\\$1/g;
         ($line_start = $line)   =~ s/($split_regex.+?)$//;
         $line_start             =~ s/([\t\s]+$)//;
         $longest_line           = length($line_start) if length($line_start) > $longest_line;
@@ -22,15 +22,15 @@ if($#lines > 0) {
 
     foreach $line (@lines) {
         $line_number++;
-        
-        if($line =~ m/.+=/) {
-            ($equals_operator)      = ($line =~ m/([\.\+\-]?=[\>\&\~]?)/);
+
+        if($line =~ m/.+[=\:]/) {
+            ($equals_operator)      = ($line =~ m/([\.\+\-]?[=\:][\>\&\~]?)/);
             ($split_regex           = $equals_operator) =~ s/([\.\+\-\=\>\&])/\\$1/g;
             ($line_start = $line)   =~ s/$split_regex.+?$//;
             ($line_end   = $line)   =~ s/^.+?$split_regex//;
             $line_start             =~ s/([\s]+$)//;
             $line_end               =~ s/^[\s]+//;
-            $padding                = ($line_number == 1) ? $longest_line - $ENV{CODA_LINE_INDEX} : $longest_line;            
+            $padding                = ($line_number == 1) ? $longest_line - $ENV{CODA_LINE_INDEX} : $longest_line;
             $padding               += $tab_width - ($longest_line % $tab_width);
             $padding               += $tab_width if $padding == $longest_line;
             
@@ -42,6 +42,6 @@ if($#lines > 0) {
             $new_output .= $line;
         }
     }
-    
+
     print $new_output;
 }
